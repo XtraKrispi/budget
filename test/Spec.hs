974ba209fe1570f190b386a7438f516131cac271
Michael Gold <mgold@spscommerce.com>
2024-10-15 21:20:24 -0400

    More tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,9 @@ import Data.Foldable (find)
 import Data.Map qualified as Map
 import Data.Maybe (isJust)
 import Data.Text.Lazy (isInfixOf, unpack)
+import Data.Text.Lazy qualified as T
+import Data.Time (fromGregorian)
+import Data.UUID qualified as UUID
 import Effectful (runPureEff)
 import Handlers (getArchive)
 import Handlers.Model (
@@ -18,10 +21,12 @@ import Handlers.Model (
  )
 import Interpreters.ArchiveStore (runArchiveStorePure)
 import Lucid (renderText)
+import Model.Archive (ArchiveAction (Paid, Skipped), ArchivedItem (..))
 import Model.Email (Email (..))
+import Model.Id qualified as Id
 import Model.Password (Password (..))
 import Model.User (User (..))
-import Test.Hspec (describe, hspec, it, shouldContain, shouldSatisfy)
+import Test.Hspec (describe, hspec, it, shouldBe, shouldContain, shouldSatisfy)
 import Text.HTML.TagSoup (Tag (..), parseTags)
 import Text.HTML.TagSoup.Tree (TagTree (..), parseTree)
 import Text.StringLike (StringLike)
@@ -74,12 +79,70 @@ main = hspec do
           let found =
                 find
                   ( \case
-                      TagText str | "No items" `isInfixOf` str -> True
+                      TagText str -> "No items" `isInfixOf` str
                       _ -> False
                   )
                   parsed
           found `shouldSatisfy` isJust
         _ -> fail "Expecting a Same Page, got a Redirect"
+    it "should render the items if there are any" do
+      let mNewId1 = Id.fromText (UUID.toText UUID.nil)
+          mNewId2 = Id.fromText (UUID.toText UUID.nil)
+      case (mNewId1, mNewId2) of
+        (Just newId1, Just newId2) -> do
+          let response =
+                runPureEff
+                  $ runArchiveStorePure
+                    ( Map.fromList
+                        [
+                          ( Email "items@email.com"
+                          ,
+                            [ ArchivedItem
+                                { archivedItemId = newId1
+                                , archivedItemItemDefinitionId = newId2
+                                , archivedItemDescription = "Test"
+                                , archivedItemAmount = 123
+                                , archivedItemDate = fromGregorian 2024 1 1
+                                , archivedItemActionDate = fromGregorian 2024 1 1
+                                , archivedItemAction = Paid
+                                }
+                            , ArchivedItem
+                                { archivedItemId = newId1
+                                , archivedItemItemDefinitionId = newId2
+                                , archivedItemDescription = "Test 1"
+                                , archivedItemAmount = 1234
+                                , archivedItemDate = fromGregorian 2024 2 1
+                                , archivedItemActionDate = fromGregorian 2024 2 1
+                                , archivedItemAction = Skipped
+                                }
+                            ]
+                          )
+                        ]
+                    )
+                  $ getArchive
+                    Request
+                      { requestCookies = []
+                      , requestHeaders = [("HX-Request", "true")]
+                      , requestParams = []
+                      }
+                    User
+                      { email = Email "items@email.com"
+                      , name = ""
+                      , passwordHash = Password ""
+                      }
+          case response of
+            SamePage (SamePageResponse{..}) -> do
+              let parsed = parseTags $ renderText samePageResponseContent
+              let allCards =
+                    filter
+                      ( \case
+                          TagOpen "div" attrs -> any (\(k, v) -> k == "class" && elem "card" (T.words v)) attrs
+                          _ -> False
+                      )
+                      parsed
+              length allCards `shouldBe` 2
+            _ -> fail "Expecting a Same Page, got a Redirect"
+        _ -> fail "This shouldn't have happened"
 
 findMatchingTags :: (StringLike str) => str -> [TagTree str] -> [TagTree str] -> [TagTree str]
 findMatchingTags tagName trees accum = do
