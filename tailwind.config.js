5efc24976da249cc759452537acd4d1798ebdae9
Michael Gold <mgold@spscommerce.com>
2024-11-04 14:07:17 -0500

    Fixing tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,hs}"],
+  content: ["./lib/**/*.{html,js,hs}", "./app/**/*.{html,js,hs}"],
   theme: {
     extend: {},
   },
